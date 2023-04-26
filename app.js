45f33616881e03ae59e0ff1afe17843bba6ccdbe
paulaBagnaroli <paula.bagnaroli@gmail.com>
2023-04-26 10:33:56 -0300

    feat: :zap: Date responsive
    Now you can choose the date you want the pics from

--- app.js
+++ app.js
@@ -10,10 +10,22 @@ btnAnterior.addEventListener("click",(e)=>{
     cargarDatos();
 })
 
+const fechaInput=document.querySelector("#fecha");
+console.log(fechaInput)
+let fecha = 2023-04;
+
+console.log(fecha);
+
+fechaInput.addEventListener("change",(e)=>{
+fecha = fechaInput.value
+console.log("la fecha es"+ fecha);
+cargarDatos(fecha)}
+)
+
 const cargarDatos= async() =>{ 
     contenedor.innerHTML=null;
     try{//Usamos fetch para acceder al contenido de la url. Al usar fetch nos devuelve una PROMESA, que va a ser almacenada en la varibale respuesta en este caso. Como es una promesa, le pones await, para que se ejecute cuando ya esté la respuesta. Await solo puede usarse en funciones asíncronas, por eso arriba a la función cargarDatos tenes que ponerle async. Además, cuando trabajas con funciones asíncronas tenes que usar try y catch.
-     const respuesta= await fetch (`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2023-4-3&page=${pagina}&api_key=nTSFEdegLBKcBA7biDMbYnLaRjkO2xdreLD4CUh3
+     const respuesta= await fetch (`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${fecha}3&page=${pagina}&api_key=nTSFEdegLBKcBA7biDMbYnLaRjkO2xdreLD4CUh3
      `)
      console.log(pagina)
      if (respuesta.status ===200){
@@ -51,7 +63,6 @@ let subtitulo = document.querySelector("h2")
 let datosRover=document.createElement(`p`)
 datosRover.textContent=`Nombre del Rover: Curiosity - Estado: activo`
 subtitulo.appendChild(datosRover)
-
 
 
 
